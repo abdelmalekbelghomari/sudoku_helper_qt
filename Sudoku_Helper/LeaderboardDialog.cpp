e19965d4ee2cec5e429d5a21e98e8a3b770bf603
sriha <haykel.sriha@ecole.ensicaen.fr>
2024-03-17 22:12:59 +0100

    the leaderboard is now a table

--- Sudoku_Helper/LeaderboardDialog.cpp
+++ Sudoku_Helper/LeaderboardDialog.cpp
@@ -2,6 +2,8 @@
 #include <QFile>
 #include <QTextStream>
 #include <QRegularExpression>
+#include <QTableWidget>
+#include <QHeaderView>
 
 LeaderboardDialog::LeaderboardDialog(QWidget *parent) : QDialog(parent) {
     QVBoxLayout *layout = new QVBoxLayout(this);
@@ -14,17 +16,46 @@ void LeaderboardDialog::loadAndDisplayData() {
     QFile file("completedLevels.txt");
     if (file.open(QIODevice::ReadOnly)) {
         QTextStream in(&file);
-        QString content = in.readAll(); // Lit tout le contenu du fichier d'un coup
 
-        // Affiche le contenu dans un QLabel
-        QLabel *label = new QLabel(content, this);
-        label->setWordWrap(true); // Permet au texte de passer à la ligne si nécessaire
-        layout()->addWidget(label);
+        // Créez la QTableWidget avec 3 colonnes
+        QTableWidget* table = new QTableWidget(this);
+        table->resize(200, 400);
+        table->setColumnCount(3); // Pour Level, Grid, et Time
+        table->setHorizontalHeaderLabels({"Level", "Grid", "Time"});
+        table->horizontalHeader()->setStretchLastSection(true);
+        table->verticalHeader()->setVisible(false);
+        table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+        table->setSelectionBehavior(QAbstractItemView::SelectRows);
+        table->setSelectionMode(QAbstractItemView::SingleSelection);
+
+        int row = 0;
+        while (!in.atEnd()) {
+            QString line = in.readLine();
+            QRegularExpression re("Level:\\s*(\\w+)\\s*Grid:\\s*(\\d+)\\s*Time:\\s*(\\S+)");
+            QRegularExpressionMatch match = re.match(line);
+            if (match.hasMatch()) {
+                table->insertRow(table->rowCount());
+                table->setItem(row, 0, new QTableWidgetItem(match.captured(1)));
+                table->setItem(row, 1, new QTableWidgetItem(match.captured(2)));
+                table->setItem(row, 2, new QTableWidgetItem(match.captured(3)));
+                ++row;
+            }
+        }
+
+        // Ajuster la taille des colonnes à leur contenu
+        table->resizeColumnsToContents();
+
+        // Définissez le layout pour inclure la table
+        QVBoxLayout *layout = new QVBoxLayout(this);
+        layout->addWidget(table);
+        this->setLayout(layout);
 
         file.close();
     } else {
         // Affiche un message si le fichier ne peut pas être ouvert ou est introuvable
         QLabel *label = new QLabel("No completed levels found.", this);
-        layout()->addWidget(label);
+        QVBoxLayout *layout = new QVBoxLayout(this);
+        layout->addWidget(label);
+        this->setLayout(layout);
     }
 }
